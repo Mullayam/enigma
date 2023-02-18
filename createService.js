bda1a32a6a52c91ff51d366ba32acd1ad148b31a
Mulayam <mullayam06@gmail.com>
2023-02-19 01:25:01 +0530

    initial commit

--- createService.js
+++ createService.js
@@ -1,12 +1,16 @@
 const crypto = require("crypto");
 const ALGORITHM = "aes-256-cbc";
 const ENCODING = "hex";
-const KEY = "enjoys_encrption_key!@#%^&*()_NJ" || process.env.ENCRYPTION_KEY;
+const ENCRYPTION_KEY = "enjoys_encrption_key!@#%^&*()_NJ" || process.env.ENCRYPTION_KEY;
 
 const key = {
   encrypt: (text, IV_LENGTH = 16) => {
     const iv = crypto.randomBytes(IV_LENGTH);
-    const cipher = crypto.createCipheriv(ALGORITHM, Buffer.from(KEY), iv);
+    const cipher = crypto.createCipheriv(
+      ALGORITHM,
+      Buffer.from(ENCRYPTION_KEY),
+      iv
+    );
     let encrypted = cipher.update(text);
     encrypted = Buffer.concat([encrypted, cipher.final()]);
     return `${iv.toString(ENCODING)}:${encrypted.toString(ENCODING)}`;
@@ -15,7 +19,11 @@ const key = {
     const textParts = text.split(":");
     const iv = Buffer.from(textParts.shift(), ENCODING);
     const encryptedText = Buffer.from(textParts.join(":"), ENCODING);
-    const decipher = crypto.createDecipheriv(ALGORITHM, Buffer.from(KEY), iv);
+    const decipher = crypto.createDecipheriv(
+      ALGORITHM,
+      Buffer.from(ENCRYPTION_KEY),
+      iv
+    );
     let decrypted = decipher.update(encryptedText);
     decrypted = Buffer.concat([decrypted, decipher.final()]);
     return decrypted.toString();
